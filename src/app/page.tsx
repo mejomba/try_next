77f9c94279d10e45be91d546e47f6dd1d0bd57d3
mejomba <moojaminzadeh@gmail.com>
2025-04-09 17:01:30 +0330

    fetch data from server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
+import axios from "axios"
 
-export default function Home() {
+export type Products = Product[]
+
+export interface Product {
+    id: number
+    title: string
+    price: number
+    description: string
+    category: string
+    image: string
+    rating: Rating
+}
+
+export interface Rating {
+    rate: number
+    count: number
+}
+
+export default async function Home() {
+    const {data} = await axios.get('http://fakestoreapi.com/products');
   return (
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
           <h1>Title</h1>
+          {
+              data.map((product: Product) => (
+                  <div key={product.id} className="flex flex-col gap-8 items-center">
+                      <h3>{product.title}</h3>
+                      <p>{product.description}</p>
+                  </div>
+              ))
+          }
       </main>
   );
 }
